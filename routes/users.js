fde4e37ecf13472817ea188812a5b1037ab742c4
Ronal Cuenca <fermindozzz@gmail.com>
2024-02-21 01:30:12 -0500

    Backend

--- routes/users.js
+++ routes/users.js
@@ -13,42 +13,48 @@ const sensorC = require('../app/controls/SensorControl');
 let sensorControl = new sensorC();
 const datosClimaC = require('../app/controls/datosClimaticosControl');
 let datosClimaControl = new datosClimaC();
+const PrediccionControl = require('../app/controls/PrediccionControl');
+let prediccionControl = new PrediccionControl();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('reporte de quinto');
 });
 
-var auth = function middleware(req, res, next) {
-  const token = req.headers["TOKEN-API"];
-  if (token) {
-    require("dotenv").config();
-    const llave = process.env.KEY_PRI;
-    jwt.verify(token, llave, async (err, decoded) => {
-      if (err) {
-        res.status(401);
-        res.json({ msg: "token expirado o no valido", code: 401 });
-      } else {
-        var models = require("../app/models");
-        req.decoded = decoded;
-        let aux = await models.cuenta.findOne({
-          where: { external_id: req.decoded.external },
+//MIDDLEWARE
+const auth = function middleware(req, res, next){
+  //AUTENTICACION
+  const token = req.headers['pis-token'];
+  if(token === undefined){
+    res.status(400);
+    res.json({ msg: "ERROR", tag: "Falta token", code: 400 });
+  }else{
+    require('dotenv').config();
+    const key = process.env.KEY_JWT;
+    jwt.verify(token, key, async(error, decoded) => {
+      if(error){
+        res.status(400);
+        res.json({ msg: "ERROR", tag: "TOKEN NO VALIDO O EXPIRADO", code: 400 });
+      }else{
+        console.log(decoded.external);
+        const models = require('../app/models');
+        const cuenta = models.cuenta;
+        const aux = await cuenta.findOne({
+          where: {external_id : decoded.external}
         });
-        if (!aux) {
-          res.status(401);
-          res.json({ msg: "token no valido", code: 401 });
-        } else {
+        if(aux === null){
+          res.status(400);
+        res.json({ msg: "ERROR", tag: "TOKEN NO VALIDO", code: 400 });
+        }else{
+          //AUTORIZACION
           next();
         }
       }
+      
     });
-  } else {
-    res.status(401);
-    res.json({ msg: "No existe token", code: 401 });
+    
   }
-};
-
-
+}
 
 //router.get('/hola', function(req, res, next) {
   //console.log(req);
@@ -56,30 +62,34 @@ var auth = function middleware(req, res, next) {
   //res.render('index', { title: 'Express' });
 //});
 
-router.get('/admin/personas', personaControl.listar);
-router.get('/admin/personas/get/:external', personaControl.obtener);
-router.post('/admin/personas/save', personaControl.guardar);
-router.put('/admin/personas/put/:external', personaControl.modificar);
-router.get('/admin/personas/cambiarEstado/:external/:nuevoEstado', personaControl.cambiarEstado);
+router.get('/admin/personas', auth, personaControl.listar);
+router.get('/admin/personas/get/:external', auth, personaControl.obtener);
+router.post('/admin/personas/save', auth,  personaControl.guardar);
+router.put('/admin/personas/put/:external', auth, personaControl.modificar);
+router.get('/admin/personas/cambiarEstado/:external/:nuevoEstado', auth, personaControl.cambiarEstado);
 
-router.get('/admin/rol', rolControl.listar);
-router.post('/admin/rol/save', rolControl.guardar);
+router.get('/admin/rol', auth, rolControl.listar);
+router.post('/admin/rol/save', auth, rolControl.guardar);
 
-router.get('/admin/mota', motaControl.listar);
-router.post('/admin/mota/save', motaControl.guardar);
-router.get('/admin/mota/get/:external', motaControl.obtener);
-router.get('/admin/mota/cambiarEstado/:external/:nuevoEstado', motaControl.cambiarEstado);
-router.put('/admin/mota/put/:external', motaControl.modificar);
+router.get('/admin/mota', auth, motaControl.listar);
+router.post('/admin/mota/save', auth, motaControl.guardar);
+router.get('/admin/mota/get/:external', auth, motaControl.obtener);
+router.get('/admin/mota/cambiarEstado/:external/:nuevoEstado', auth, motaControl.cambiarEstado);
+router.put('/admin/mota/put/:external', auth, motaControl.modificar);
 
-router.get('/admin/sensor', sensorControl.listar);
-router.post('/admin/sensor/save', sensorControl.guardar);
-router.get('/admin/sensor/cambiarEstado/:external/:nuevoEstado', sensorControl.cambiarEstado);
-router.get('/admin/sensor/get/:external', sensorControl.obtener);
-router.put('/admin/sensor/put/:external', sensorControl.modificar);
+router.get('/admin/sensor', auth, sensorControl.listar);
+router.post('/admin/sensor/save', auth, sensorControl.guardar);
+router.get('/admin/sensor/cambiarEstado/:external/:nuevoEstado', auth, sensorControl.cambiarEstado);
+router.get('/admin/sensor/get/:external', auth, sensorControl.obtener);
+router.put('/admin/sensor/put/:external', auth, sensorControl.modificar);
 
 router.get('/admin/datosClimaticos', datosClimaControl.listar);
 router.post('/admin/datosClimaticos/save', datosClimaControl.guardar);
 
+router.post('/admin/prediccionClimatica/extrapolarPresion', prediccionControl.extapolacionLinealPresion);
+router.post('/admin/prediccionClimatica/extrapolarHumedad', prediccionControl.extapolacionLinealhumedad);
+router.post('/admin/prediccionClimatica/extrapolarTemp', prediccionControl.extapolacionLinealTemperatura);
+router.get('/admin/prediccion', prediccionControl.listar);
 
 router.post('/login', cuentaControl.inicio_sesion);
 module.exports = router;
